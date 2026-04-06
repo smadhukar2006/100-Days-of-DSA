32f8d82f5093d7e0a387dc6d5e8afc0a5ead9441
smadhukar2006 <smadhukar2006@gmail.com>
2026-04-06 19:59:48 +0530

    Create day9.c

---
+++ day9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[1000]; 
+    scanf("%s", str);
+
+    int len = strlen(str);
+
+    for (int i = len - 1; i >= 0; i--) {
+        printf("%c", str[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
