5560aca37955dd4a791b3b592a8caacdbcb6280e
smadhukar2006 <smadhukar2006@gmail.com>
2026-04-06 20:27:06 +0530

    Create day15.c

---
+++ day15.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+    scanf("%d %d", &m, &n);
+
+    int mat[m][n];
+    int sum = 0;
+
+    
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &mat[i][j]);
+            if (i == j) {  
+                sum += mat[i][j];
+            }
+        }
+    }
+
+    printf("%d\n", sum);
+
+    return 0;
+}
