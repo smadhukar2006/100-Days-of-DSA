edf4bded1a448f3d4592220bae2e53264fc8149b
smadhukar2006 <smadhukar2006@gmail.com>
2026-04-06 20:53:14 +0530

    Create day22.c

---
+++ day22.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+
+int countNodes(struct Node* head) {
+    int count = 0;
+    struct Node* temp = head;
+    while (temp != NULL) {
+        count++;
+        temp = temp->next;
+    }
+    return count;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    struct Node* head = NULL;
+    struct Node* tail = NULL;
+
+   
+    for (int i = 0; i < n; i++) {
+        int value;
+        scanf("%d", &value);
+        struct Node* newNode = createNode(value);
+
+        if (head == NULL) {
+            head = newNode;  
+            tail = newNode;
+        } else {
+            tail->next = newNode;  
+            tail = newNode;
+        }
+    }
+
+   
+    int totalNodes = countNodes(head);
+    printf("Number of nodes = %d\n", totalNodes);
+
+    return 0;
+}
