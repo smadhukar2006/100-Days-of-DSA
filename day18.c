2b05a3a71ec59530b33ff02b9f9f11a8417a9200
smadhukar2006 <smadhukar2006@gmail.com>
2026-04-06 20:38:54 +0530

    Create day18.c

---
+++ day18.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &k);
+
+   
+    k = k % n;
+
+    int rotated[n];
+    int index = 0;
+
+   
+    for (int i = n - k; i < n; i++) {
+        rotated[index++] = arr[i];
+    }
+
+    
+    for (int i = 0; i < n - k; i++) {
+        rotated[index++] = arr[i];
+    }
+
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", rotated[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
